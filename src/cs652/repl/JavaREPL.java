4de2af841669a3b63b8cf9180ec316d3f5a2a2e1
Mayank Thirani <mthirani@dons.usfca.edu>
2016-01-30 15:38:32 -0800

    Java REPL v1.0
    1. Nested Reader Implemented
    2. Java REPL Class implemented
    3. Errors still popping in some test cases

--- src/cs652/repl/JavaREPL.java
+++ src/cs652/repl/JavaREPL.java
@@ -1,20 +1,140 @@
 package cs652.repl;
 
-import java.io.IOException;
+import com.sun.source.util.JavacTask;
+import javax.tools.*;
+import java.io.*;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
+class HandlingCompilerOperations {
+    DiagnosticCollector dg;
+    JavacTask javaTask;
+    public HandlingCompilerOperations(DiagnosticCollector d, JavacTask jT){
+        dg = d;
+        javaTask = jT;
+    }
+}
 public class JavaREPL {
-	public static void main(String[] args) throws IOException {
-		exec(new InputStreamReader(System.in));
-	}
 
-	public static void exec(Reader r) throws IOException {
-		BufferedReader stdin = new BufferedReader(r);
-		NestedReader reader = new NestedReader(stdin);
-		int classNumber = 0;
-		while (true) {
-			System.out.print("> ");
-			String java = reader.getNestedString();
-			// TODO
-		}
-	}
+    public static final String tempDir = System.getProperty("java.io.tmpdir");
+
+    public static void main(String[] args) throws IOException {
+        exec(new InputStreamReader(System.in));
+    }
+
+    public static void exec(Reader r) throws IOException {
+        BufferedReader stdin = new BufferedReader(r);
+        NestedReader reader = new NestedReader(stdin);
+        int classNumber = 0;
+        while (true) {
+            System.out.print("> ");
+            String readStr = reader.getNestedString();
+            if (readStr == null)
+                return;
+            readStr = readStr.trim();
+            if(readStr.startsWith("print") && readStr.endsWith(";")){
+                int index = readStr.lastIndexOf(";");
+                String printStat = readStr.substring(0, index);
+                String printStatement = "System.out.println(" + printStat.substring("print ".length()) + ");";
+                readStr = printStatement;
+            }
+            if(readStr.equals(""))
+                continue;
+            String tempFileName = "Test.java";
+            String definition = "public static " + readStr;
+            String statement = "";
+            String code = getCode("Test", "", "", definition, "");
+            writeToFile(tempFileName, code);
+            String errorParse = compileCode(tempFileName, "parse");
+            if(errorParse != null){
+                statement = readStr;
+                definition = "";
+            }
+            String nameClass = "Interp_" + classNumber;
+            int superClassNumber = classNumber - 1;
+            String superClass = "";
+            if(classNumber > 0) {
+                superClass = "Interp_" + superClassNumber;
+                code = getCode(nameClass, " extends ", superClass, definition, statement);
+            }
+            else
+                code = getCode(nameClass, "", "", definition, statement);
+            String fileName = nameClass + ".java";
+            writeToFile(fileName, code);
+            String errorCompile = compileCode(fileName, "compile");
+            if(errorCompile != null){
+                System.err.print(errorCompile);
+            }
+            else{
+                exec(nameClass);
+            }
+            classNumber++;
+        }
+    }
+
+    public static String getCode(String nameClass, String extend, String superClass, String definition, String statement)
+    {
+        String javaCode = "import java.io.*;\n" + "import java.util.*;\n"+
+                "public class " + nameClass + extend + superClass + "{\n" + definition + "\n" +
+                "public static void exec() {\n" + statement + "\n" + "}\n" + "}\n";
+
+        return javaCode;
+    }
+
+    public static String errorMessage(HandlingCompilerOperations hc, String fileName) throws IOException {
+        StringBuilder sbuf = new StringBuilder(100);
+        if(hc.dg.getDiagnostics().size() == 0)
+            return null;
+        List<Diagnostic> al = hc.dg.getDiagnostics();
+        for(Diagnostic d: al){
+            sbuf.append("line ").append(d.getLineNumber()).append(": ").append(d.getMessage(Locale.ENGLISH)).append("\n");
+        }
+
+        return sbuf.toString();
+    }
+
+    public static String compileCode(String fileName, String option) throws IOException{
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        DiagnosticCollector diagnosticsCollector = new DiagnosticCollector();
+        StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnosticsCollector, null, null);
+        String dirFile = tempDir + File.separator + fileName;
+        Iterable fileObjects = fileManager.getJavaFileObjectsFromStrings(Arrays.asList(dirFile));
+        String classPath = "." + ";" + tempDir;
+        Iterable<String> options = Arrays.asList("-d", tempDir, "-cp", classPath);
+        JavacTask task = (JavacTask) compiler.getTask(null, fileManager, diagnosticsCollector, options, null, fileObjects);
+        HandlingCompilerOperations hc = new HandlingCompilerOperations(diagnosticsCollector, task);
+        if(option.equals("parse"))
+            hc.javaTask.parse();
+        else
+            hc.javaTask.call();
+
+        return errorMessage(hc, fileName);
+    }
+
+    public static void writeToFile(String fileName, String code){
+        String dirFile = tempDir + File.separator + fileName;
+        try(BufferedWriter bufWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(dirFile))))
+        {
+            bufWriter.write(code);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void exec(String nameClass) {
+        try{
+            ClassLoader parentClassLoading = ClassLoader.getSystemClassLoader();
+            URL url = new File(tempDir).toURI().toURL();
+            ClassLoader classLoader = new URLClassLoader(new URL[] {url}, parentClassLoading);
+            Class actualClass = classLoader.loadClass(nameClass);
+            Method decMeth = actualClass.getDeclaredMethod("exec", (Class[])null);
+            decMeth.invoke(null, (Object[]) null);
+        }catch(Exception e){
+            e.printStackTrace(System.err);
+        }
+    }
 }
---
+++ src/cs652/repl/NestedReader.java
@@ -0,0 +1,105 @@
+package cs652.repl;
+
+/**
+ * Created by Mayank on 1/28/2016.
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Stack;
+
+public class NestedReader {
+    StringBuilder buf;
+    BufferedReader input;
+    char c;
+
+    public NestedReader(BufferedReader input) {
+        this.input = input;
+    }
+
+    public String getNestedString() throws IOException {
+        buf = new StringBuilder();
+        Stack<Character> inputChars = new Stack<Character>();
+        c = (char )input.read();
+        while(true){
+            if(c == (char)-1)
+                return null;
+            if(c == '{' || c == '(' || c == '[') {
+                inputChars.push(c);
+                buf.append(c);
+            }
+            else if (c == '/'){
+                c = (char )input.read();
+                if (c != '/')
+                {
+                    buf.append('/');
+                    continue;
+                }
+                ignoreAllChars(c);
+                if(inputChars.size() == 0)
+                    return buf.toString();
+            }
+            else if (c == '}') {
+                if (inputChars.size() == 0 || inputChars.lastElement() != '{') {
+                    appendAllChars(c);
+                    return buf.toString();
+                }
+                buf.append(c);
+                inputChars.pop();
+            }
+            else if (c == ']') {
+                if (inputChars.size() == 0 || inputChars.lastElement() != '[') {
+                    appendAllChars(c);
+                    return buf.toString();
+                }
+                buf.append(c);
+                inputChars.pop();
+            }
+            else if (c == ')') {
+                if (inputChars.size() == 0 || inputChars.lastElement() != '(') {
+                    appendAllChars(c);
+                    return buf.toString();
+                }
+                buf.append(c);
+                inputChars.pop();
+            }
+            else if (c == '"'){
+                if(inputChars.size() == 0) {
+                    appendAllChars(c);
+                    return buf.toString();
+                }
+                buf.append(c);
+            }
+            else if (c == '\n'){
+                if(inputChars.size() == 0){
+                    return buf.toString();
+                }
+                buf.append(c);
+            }
+            else{
+                buf.append(c);
+            }
+            c = (char )input.read();
+        }
+    }
+
+    public void appendAllChars(char c) throws IOException{
+        while(c != '\n') {
+            if(c == '/'){
+                c = (char )input.read();
+                if(c == '/'){
+                    ignoreAllChars(c);
+                    break;
+                }
+                buf.append('/');
+            }
+            buf.append(c);
+            c = (char )input.read();
+        }
+    }
+
+    public void ignoreAllChars(char c) throws IOException{
+        while(c != '\n') {
+            c = (char )input.read();
+        }
+    }
+}
